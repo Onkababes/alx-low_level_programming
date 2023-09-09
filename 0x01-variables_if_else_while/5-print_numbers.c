46f9d3778bba2aae2183c7fcd6a3c26a67b63d47
Onkababes <onkamokgwasa@icloud.com>
2023-09-09 10:04:55 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers zero to nine
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10 ; k++)
+		printf("%d", k);
+	printf("\n");
+	return (0);
+}
