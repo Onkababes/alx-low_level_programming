4d257f4a24de6aa7dca6067361c4f257a8ca566f
Onkababes <onkamokgwasa@icloud.com>
2023-09-10 16:18:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - A printf file
  * Return: 0 (possibility)
@@ -6,7 +7,7 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
