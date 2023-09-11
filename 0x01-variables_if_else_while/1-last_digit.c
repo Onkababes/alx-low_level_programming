4b1f6cdf59c43556241c4610cbc17220c4ad10d6
Onkababes <onkamokgwasa@icloud.com>
2023-09-11 14:59:59 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,16 +17,15 @@ int main(void)
 	o = n % 10;
 	if (o > 5)
 	{
-		printf("Last digit of %d is %d and greater than 5", n, o);
+		printf("Last digit of %d is %d and is greater than 5\n", n, o);
 	}
 	else if (o == 0)
 	{
-		printf("Last digit of %d is %d and is zero", n, o);
+		printf("Last digit of %d is %d and is 0\n", n, o);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, o);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, o);
 	}
-	printf("\n");
 	return (0);
 }
