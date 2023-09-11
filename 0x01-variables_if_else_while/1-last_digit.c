7778de24a16d22e00d548082e8f3f5cb4afac3ef
Onkababes <onkamokgwasa@icloud.com>
2023-09-11 14:50:36 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,26 +3,30 @@
 #include <stdio.h>
 
 /**
- *
- * main - Reveal the last digit
- *
+ * main - Write the only the last digit of the of the answer
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-        int n;
-        int o;
+	int n;
+	int o;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        o = n % 10;
-        if (o < 5)
-                printf("Last digit of %d is %d and greater than 5", n, o);
-        else if (o == 0
-                printf("Last digit of %d is %d and is zero", n, o);
-        else if(o < 6 && o != 0)
-                printf("Last digit of %d is %d and is less than 6 and not 0", n, o);
-
-        printf("\n");
-        return (0);
-}i
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	o = n % 10;
+	if (o > 5)
+	{
+		printf("Last digit of %d is %d and greater than 5", n, o);
+	}
+	else if (o == 0)
+	{
+		printf("Last digit of %d is %d and is zero", n, o);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, o);
+	}
+	printf("\n");
+	return (0);
+}
