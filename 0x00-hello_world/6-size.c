aadb4397c28f7e9d3182dc4639b7a234a7716cd0
Onkababes <onkamokgwasa@icloud.com>
2023-09-11 19:49:15 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of char: %lu bytes\n", sizeof(a));
-	printf("Size of int: %lu bytes\n", sizeof(b));
-	printf("Size of long: %lu bytes\n", sizeof(c));
-	printf("Size of long long int: %lu bytes\n", sizeof(d));
-	printf("Size of float: %lu bytes\n", sizeof(e));
+	printf("Size of char: %lu byte(s)\n", sizeof(a));
+	printf("Size of int: %lu byte(s)\n", sizeof(b));
+	printf("Size of long: %lu byte(s)\n", sizeof(c));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of float: %lu byte(s)\n", sizeof(e));
 	return (0);
 }
