43fa25f27f1105260620cbc8c838cc0449c6d359
Onkababes <onkamokgwasa@icloud.com>
2023-09-11 19:25:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - Sizes a of computers
+ *
  * Return: 0 (success)
  */
 
 int main(void)
 {
-	char k;
-	int l;
-	long n;
-	long long int o;
-	float m;
+	char a;
+	int b;
+	long c;
+	long long int d;
+	float e;
 
-	printf("Size of char: %zu bytes\n", (unsigned long)sizeof(k));
-	printf("Size of int: %zu bytes\n", (unsigned long)sizeof(l));
-	printf("Size of long: %zu bytes\n", (unsigned long)sizeof(n));
-	printf("Size of long long int: %zu bytes\n", (unsigned long)sizeof(o));
-	printf("Size of float: %zu bytes\n", (unsigned long)sizeof(m));
+	printf("Size of char: %lu bytes\n", (unsigned long)sizeof(a));
+	printf("Size of int: %lu bytes\n", (unsigned long)sizeof(b));
+	printf("Size of long: %lu bytes\n", (unsigned long)sizeof(c));
+	printf("Size of long long int: %lu bytes\n", (unsigned long)sizeof(d));
+	printf("Size of float: %lu bytes\n", (unsigned long)sizeof(e));
 	return (0);
 }
