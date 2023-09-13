410bebce437f258c08c92cc23972768a476bc98e
Onkababes <onkamokgwasa@gmail.com>
2023-09-12 19:12:34 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - lower cases alphabets
+ * Return: 0 Success
+ */
+void print_alphabet(void)
+{
+
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
