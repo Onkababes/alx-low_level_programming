2cac01dbc1a9063f0e40aea426ac090c3730e7f3
Onkababes <onkamokgwasa@gmail.com>
2023-09-14 13:41:26 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,17 +3,14 @@
 /**
  * _isdigit -a funtion that checks for a digit
  * @c: The alphabet to concider within a digit
- * Return: 1 if is is a digit otherwise 0
+ * Return: 1 if c is a digit otherwise 0
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 	{
-		return (1);
+	return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
