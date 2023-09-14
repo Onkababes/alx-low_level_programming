3054333bfb26277ccbf7341663247b7ab6817962
Onkababes <onkamokgwasa@gmail.com>
2023-09-14 14:57:53 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - functions that checks for digit 0 to 9
+ *
+ * Return: 0 always
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+}
