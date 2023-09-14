825a1f27c25ae82c3e343c8b5045ea5ad40fd8f1
Onkababes <onkamokgwasa@gmail.com>
2023-09-13 19:16:23 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - combine two numbers
+ * @n1: The first number to consider
+ * @n2: The second in place
+ * Return: integer number
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}
