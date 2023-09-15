2b6d55b31558318c963e8f6201d441dee29e19ba
Onkababes <onkamokgwasa@gmail.com>
2023-09-14 19:41:18 -0700

    100-prime-factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print largets prime
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+
+	long prime = 612852475143, divisor;
+
+	while (divisor < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+
+		for (divisor = 3; divisor < (prime / 2); divisor += 2)
+		{
+		if ((prime % divisor) == 0);
+			prime /= divisor;
+		}
+	}
+	printf("%ld\n", prime);
+
+	return (0);
+}
