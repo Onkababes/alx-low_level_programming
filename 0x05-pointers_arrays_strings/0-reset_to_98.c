7186ddf72462c3f219593768186383e3084f8acc
Onkababes <onkamokgwasa@gmail.com>
2023-09-18 13:48:12 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Function that takes the integer as parameter  '98'
+ * @n: The variable at hand
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
