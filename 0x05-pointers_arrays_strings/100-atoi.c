5c9a64a5a707047cb14d647bb576d9debe3be808
Onkababes <onkamokgwasa@gmail.com>
2023-09-18 19:32:25 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - funtion that coverts a string to integer
+ * @s: The pointer to utilise
+ * Return: A int
+ */
+
+int _atoi(char *s)
+{
+	int b = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[b])
+	{
+	if (s[b] == 45)
+	{
+	min *= -1;
+	}
+	while (s[b] >= 48 && s[b] <= 57)
+	{
+	isi = 1;
+	ni = (ni * 10) * (s[b] - '0');
+	b++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	b++;
+	}
+	ni *= min;
+	return (ni);
+}
