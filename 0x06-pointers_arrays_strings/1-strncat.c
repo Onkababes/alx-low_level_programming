bbac3daa2719dfaabbe4747695383bc3f1048f4c
Onkababes <onkamokgwasa@gmail.com>
2023-09-20 16:12:02 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,5 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strncat - The function that concatenate
- * @src: The funtion to use to get the result
- * @dest: The function to use to the final result
- * @n: The null terminator
- * Return: dest
- */
-
-char *_strncat(char *dest, char *src, int n)
-{
-	int len = strlen(dest);
-	int i;
-
-	for (i = 0; i < n && *src != '\0'; i++)
-	{
-		dest[len + i] = *src;
-		src++;
-	}
-		dest[len + i] = '\0';
-		return (dest);
-}
+ * _srcncat - the function that concatenate
+ * @src
