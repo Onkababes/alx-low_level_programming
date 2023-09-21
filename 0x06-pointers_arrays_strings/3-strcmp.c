292b6ad6f7a422ec1a14b1e9a65496acddc487f3
Onkababes <onkamokgwasa@gmail.com>
2023-09-20 17:50:57 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	i = 0;
-
-	while (s1[1] != '\0' && s2[i] != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] != s2[2])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+	s1++;
+	s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
