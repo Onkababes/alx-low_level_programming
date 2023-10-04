d31aa79f93e22036cfa473ff5a171e55fe60415a
Onkababes <onkamokgwasa@gmail.com>
2023-10-03 17:57:56 -0700

    had to commit main.h only

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,4 +2,19 @@
 
 /**
  * _srcncat - the function that concatenate
- * @src
+ * @src: The string to combine
+ * @dest: Another string
+ * @n: the bytes storer
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_length =0;
+
+	while (dest[index++])
+	dest_length++;
+	for (index = 0; src[index] && index < n; index++)
+	dest[dest_length++] = src[index];
+	return (dest);
+}
