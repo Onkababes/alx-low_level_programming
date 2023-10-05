d575e21d5aab4ab4151f9945f8349a40188a5f4f
Onkababes <onkamokgwasa@gmail.com>
2023-10-04 17:04:17 -0700

    0-whatmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name
+ *
+ * @argc:  argument that commands
+ * @argv: argument
+ * return: 0
+ */
+
+int main(int agrc __attribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
