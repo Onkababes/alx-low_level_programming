10bfb343fa810c5d3960dfb068b56d5a6653f28c
Onkababes <onkamokgwasa@gmail.com>
2023-10-09 18:34:11 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ * File name: 3-function_like_macro
+ * @x: value of a number
+ * Description: function that computes the value of number
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
