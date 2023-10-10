7a579e9eacb3a0bfd5b8004dafd2fee5949fc3b2
Onkababes <onkamokgwasa@gmail.com>
2023-10-09 18:42:13 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File name: 4-sum.h
+ * @x: the first number
+ * @y: the second number
+ * Description: funvtion that computes sum of numbers
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
