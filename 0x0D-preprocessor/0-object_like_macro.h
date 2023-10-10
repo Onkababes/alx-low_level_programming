ce6efd9e2c8e36c420f8a1cfc980c255c399d492
Onkababes <onkamokgwasa@gmail.com>
2023-10-09 17:57:07 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,9 +2,10 @@
 #define MACRO_H
 
 /**
- * File name: objest like macro
- * Description: a header file that creates a size abbreviated
+ * File: 0-object_like_macro
+ * Desc: a header file that creates a size abbreviated
  */
+
 #define SIZE 1024
 
 #endif
