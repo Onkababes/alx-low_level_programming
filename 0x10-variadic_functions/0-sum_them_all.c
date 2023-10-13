c5609207d76343625d697b14fdabc3f933236a57
Onkababes <onkamokgwasa@gmail.com>
2023-10-13 14:34:07 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all - function that returns the sum of its par
+ * @n: the used sum
+ * Return: 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
