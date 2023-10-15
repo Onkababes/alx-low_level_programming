1995ae8fd689a10de5e50be229c8c9b32325a080
Onkababes <onkamokgwasa@gmail.com>
2023-10-15 02:11:38 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 
 /**
  * sum_them_all - function that returns the sum of its par
@@ -9,4 +8,24 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int sum = 0;
+	unsigned int i;
 
+	va_list keep;
+
+	va_start(keep, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			sum += va_arg(keep, const unsigned int);
+		}
+	}
+	va_end(keep);
+	return (sum);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,9 @@
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC_FUNCTIONS_H
+#ifndef _VARIADIC_FUNCTIONS
+#define _VARIADIC_FUNCTIONS
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
