bee783508833c17992334a68fa5ab16736d9dfdb
Onkababes <onkamokgwasa@gmail.com>
2023-10-15 10:10:14 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - function that documents a structure
+ * @name: what is the name og the dog
+ * @owner: who theowner of a dog is
+ * @age: the age of the dog
+ * Description: the dog header
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
