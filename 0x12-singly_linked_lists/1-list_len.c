ecf0b2ac6de8cfa5a485f69051a949fe7f402769
Onkababes <onkamokgwasa@gmail.com>
2023-10-19 06:33:13 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements.
+ * @h: head of the node
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+	return (0);
+
+	for (i = 1; h->next != NULL; i++)
+		h = h->next;
+	return (i);
+}
+
