436e9be02e7196aeac65b8c8ec7f9f4952d4eac2
Onkababes <onkamokgwasa@gmail.com>
2023-10-19 15:57:34 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * release - The functiond that prints the sentence
+ */
+
+void release(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
