bb6178cf2f7673cb5c4860d8eb161607729e4c90
Onkababes <onkamokgwasa@gmail.com>
2023-10-23 08:53:12 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - funtion that prints all the elements
+ * @h: the head of the function
+ * Return: the results
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t list = 0;
+
+	while (h)
+	{
+		list++;
+		printf("%d/n", h->n);
+		h = h->next;
+	}
+
+	return (list);
+}
