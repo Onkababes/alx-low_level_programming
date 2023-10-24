2556a44f45360f9d36b4bd08e9e61c44a335117a
Onkababes <onkamokgwasa@gmail.com>
2023-10-23 17:26:32 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,21 @@
+#include "lists.h"
 
+/**
+ * pop_listint - function that deletes the node
+ * @head: a pointer
+ * Return: 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *rem;
+	int a;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	rem = *head;
+	*head = rem->next;
+	a = rem->n;
+	free(rem);
+	return (a);
+}
