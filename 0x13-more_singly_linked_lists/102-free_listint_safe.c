84eaf4a6a8dc07a31840751e043912d8b8b55f55
Onkababes <onkamokgwasa@gmail.com>
2023-10-25 13:16:00 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,67 +1,36 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * let_listp - function that frees a list
- * @head: pointer
- * Return: nothing
+ * free_listint_safe - Frees a listint_t list
+ * @h: pointer to the head of the list
+ * Return: The size of the list
  */
-
-void let_listp(listp_t **head)
-{
-	listp_t *limi, *now;
-
-	if (head != NULL)
-	{
-		now = *head;
-		while ((limi = now) != NULL)
-		{
-			now = now->next;
-			free(limi);
-		}
-
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - function that frees the list
- * @h: the pointer
- * Return: number of nodes
- */
-
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nodes = 0;
-	listp_t *on = NULL, *nch, *tl;
+	listint_t *cur, *next;
+	size_t cou = 0;
 
-	while (*h != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	cur = *h;
+
+	while (cur != NULL)
 	{
-		nch = malloc(sizeof(listp_t));
+		next = cur->next;
+		free(cur);
+		cou++;
 
-		if (nch == NULL)
-		exit(98);
-
-		nch->p = (void *)*h;
-		nch->next = on;
-		on = nch;
-		tl = on;
-
-		while (tl->next != NULL)
+		if (next >= cur)
 		{
-			tl = tl->next;
-			if (*h == (listint_t *)tl->p)
-			{
-				printf("-> [%p] %d\n", (void *)*h, (*h)->n);
-				free_listp(&on);
-				return (nodes);
-			}
+			*h = NULL;
+			return (cou);
 		}
-		printf("[%p] %d\n", (void *)*h, (*h)->n);
-		*h = (*h)->next;
-		nodes++;
+
+		cur = next;
 	}
-	free_listp(&on);
-	return (nodes);
+
+	*h = NULL;
+	return (cou);
 }
